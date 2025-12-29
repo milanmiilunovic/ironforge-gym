e974c79b7f0d052e92220e12cc5da10907531410
milanmiilunovic <milan.milunovic@stu.ibu.edu.ba>
2025-12-29 01:46:18 +0100

    Fixing bug

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-   PROJECT_BASE_URL: "https://lobster-app-qow7u.ondigitalocean,app",
+   PROJECT_BASE_URL: "https://lobster-app-qow7u.ondigitalocean.app/",
    USER_ROLE: "user",
    ADMIN_ROLE: "admin"
 };
